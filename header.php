ca0c711d8e6ef3e9fdb21b517b9bbc94086b6ce1
Fritz <fre.healy@gmail.com>
2016-10-10 10:30:32 -0400

    finalizing header'

--- header.php
+++ header.php
@@ -17,31 +17,39 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'acstarter' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="wrapper">
-			
-			<?php if(is_home()) { ?>
-	            <h1 class="logo">
-	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-	            </h1>
-	        <?php } else { ?>
-	            <div class="logo">
-	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-	            </div>
-	        <?php } ?>
-
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'acstarter' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
-	</div><!-- wrapper -->
+        <div class="row-1">
+            <div class="wrapper full-width-wrapper clear-bottom">
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+                </nav><!-- #site-navigation -->
+                <?php $account_link = get_field("account_link","option");
+                $account_text = get_field("account_text","option");
+                if($account_text&&$account_link):?>
+                    <div class="account button">
+                        <?php echo $account_text; ?>
+                        <a href="<?php echo $account_link;?>" class="surrounding"></a>
+                    </div>
+                <?php endif;?>
+            </div><!--.wrapper-->
+        </div><!--.row-1-->
+        <div class="row-2">
+            <div class="wrapper full-width-wrapper">
+                <?php $logo = get_field("logo","option");
+                if($logo):?>
+                    <div class="logo">
+                        <img src="<?php echo $logo['url'];?>" alt="<?php echo $logo['alt'];?>">
+                        <a href="<?php echo get_bloginfo("url");?>" class="surrounding"></a>
+                    </div><!--.logo-->
+                <?php endif;?>
+            </div><!-- wrapper -->
+        </div><!--.row-2-->
 	</header><!-- #masthead -->
-
 	<div id="content" class="site-content wrapper">
---
+++ template-parts/content-index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying page content in index.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-index"); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content();
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
