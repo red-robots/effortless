54e73aaa2a7ef054202997003f70df2a5ead6938
Fritz <fre.healy@gmail.com>
2018-02-21 12:47:58 -0500

    hiding dish page and adding login and styling main header

--- pages/page-blog-2.php
+++ pages/page-blog-2.php
@@ -12,7 +12,12 @@
  * @package ACStarter
  */
 
-get_header(); ?>
+if( !in_array( 'administrator', wp_get_current_user()->roles)&&!in_array( 'subscriber', wp_get_current_user()->roles) ){
+	$redirect_to = $_SERVER['REQUEST_URI'];
+	wp_redirect(esc_url(add_query_arg( 'redirect_to', $redirect_to , get_permalink( get_option('woocommerce_myaccount_page_id')))));
+	exit;
+}
+get_header("login"); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
