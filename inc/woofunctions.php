82ec494f3a7ba8c518467b1629d17a050badc3f2
lisadebona <annalisa.quiamco@gmail.com>
2019-03-01 23:32:08 +0800

    Added custom field for text on top of login form

--- inc/woofunctions.php
+++ inc/woofunctions.php
@@ -1,10 +1,25 @@
-<?php if(!function_exists('bella_add_customer_login_text')){
+<?php 
+if(!function_exists('bella_add_customer_login_text')){
     add_action('woocommerce_login_form_start','bella_add_customer_login_text',0);
     function bella_add_customer_login_text(){
-        echo 'You need to have a membership to access the member\'s section.
-        To become a member, purchase your membership <a href="'.get_the_permalink(11).'">here</a>';
+        $text = get_field('login_text','option');
+        if($text) {
+            echo '<div class="logintoptxt">' . $text . '</div>';
+        }
+       
     }
 };
+
+if(!function_exists('bella_add_customer_register_text')){
+    add_action('woocommerce_register_form_start','bella_add_customer_register_text',0);
+    function bella_add_customer_register_text(){
+        $text = get_field('register_text','option');
+        if($text) {
+            echo '<div class="logintoptxt">' . $text . '</div>';
+        }
+    }
+};
+
 if(!function_exists('bella_remove_hooks')){
     add_action('init','bella_remove_hooks', 10);
     function bella_remove_hooks(){
